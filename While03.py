77f13f65fd94b0d8b911b4e8f44a497688344cac
Farrux <ulugbekovfarrux0822@gmail.com>
2022-10-20 11:43:53 +0500

    03

--- While03.py
+++ While03.py
@@ -6,4 +6,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    ans=0
+    ans1=0
+    while i<len(s):
+        if s[i].isdigit():
+            ans+=1
+        if s[i].isalpha():
+            ans1+=1
+        i+=1
+    return len(s)-ans-ans1  
+print(main("@5"))
